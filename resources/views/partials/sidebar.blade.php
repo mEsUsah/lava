421f35c442326f54a20f14183b8af1f153a27bae
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-05-29 11:38:36 +0200

    sidebar replace profile image with generic icon

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -27,8 +27,8 @@
     <hr>
     <div class="dropdown">
         <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>{{ Auth::user()->name }}</strong>
+            <i class="bi bi-person-circle"></i>
+            <strong>&nbsp;{{ Auth::user()->name }}</strong>
         </a>
         <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
             <li><a class="dropdown-item" href="#">Settings</a></li>
